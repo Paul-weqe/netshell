92d4540ec22acc8ec8cd2838c4847d611699cd2c
weqe <paul1tw1@gmail.com>
2024-05-10 16:07:15 +0300

    Snapshot. Hopefully last time we are using clap

--- src/base/icmp.rs
+++ src/base/icmp.rs
@@ -3,40 +3,44 @@ use std::str::FromStr;
 
 use pnet::datalink::{self, Channel::Ethernet};
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
+use pnet::packet::icmp::echo_reply::EchoReplyPacket;
 use pnet::packet::icmp::IcmpPacket;
 use pnet::packet::{
     icmp::{echo_request::MutableEchoRequestPacket, IcmpTypes},
     ip::IpNextHeaderProtocols,
-    ipv4::{checksum, Ipv4Flags, Ipv4Packet, MutableIpv4Packet},
+    ipv4::{checksum as ip_checksum, Ipv4Flags, Ipv4Packet, MutableIpv4Packet},
     Packet
 };
 use pnet::util::{checksum as util_checksum, MacAddr};
-use rand::{self, Rng};
+use rand::Rng;
 
-fn main() {
-    println!("............");
-    ping("1.1.1.1");
-}
 
-fn ping(host: &str) {    
+
+pub(crate) fn ping(host: &str) {
+    
+
     let interfaces = datalink::interfaces();
     let interface = interfaces.get(1).unwrap();
-
     let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("unhandled channel type"),
+        Ok(_) => panic!("Unable to create ethernet tunnel"),
         Err(e) => panic!("Unable to create channel: {e}")
     };
 
     'ping: for seq in 0..4 {
+
+        // icmp packet
         let mut icmp_buff = [0u8; 8];
         let mut icmp_pkt = MutableEchoRequestPacket::new(&mut icmp_buff).unwrap();
-        let identifier: u16 = rand::thread_rng().gen_range(0..1000);
-        icmp_pkt.set_icmp_type(IcmpTypes::EchoRequest);
-        icmp_pkt.set_identifier(identifier);
-        icmp_pkt.set_sequence_number(seq + 1);
-        icmp_pkt.set_checksum(util_checksum(icmp_pkt.packet(), 1));
+        let identifier: u16 = rand::thread_rng().gen_range(0..1000);        
+        {
+            icmp_pkt.set_icmp_type(IcmpTypes::EchoRequest);
+            icmp_pkt.set_identifier(identifier);
+            icmp_pkt.set_sequence_number(seq + 1);
+            icmp_pkt.set_checksum(util_checksum(icmp_pkt.packet(), 1));
+        }
 
+        // ip packet
         let mut ip_buf: Vec<u8> = vec![0u8; 20 + icmp_pkt.packet().len()];
         let total_len = ip_buf.len() as u16;
         let mut ip_pkt = MutableIpv4Packet::new(&mut ip_buf[..]).unwrap();
@@ -52,11 +56,11 @@ fn ping(host: &str) {
         ip_pkt.set_ttl(255);
         ip_pkt.set_next_level_protocol(IpNextHeaderProtocols::Icmp);
         ip_pkt.set_source(Ipv4Addr::from_str(&src.to_string()).unwrap());
-        
         ip_pkt.set_destination(Ipv4Addr::from_str(host).unwrap());
-        ip_pkt.set_checksum(checksum(&ip_pkt.to_immutable()));
+        ip_pkt.set_checksum(ip_checksum(&ip_pkt.to_immutable()));
         ip_pkt.set_payload(icmp_pkt.packet());
-
+        
+        // ethernet frame
         let mut eth_buf: Vec<u8> = vec![0u8; 14 + ip_pkt.packet().len()];
         let mut eth_pkt = MutableEthernetPacket::new(&mut eth_buf[..]).unwrap();
         eth_pkt.set_source(interface.mac.unwrap());
@@ -80,9 +84,15 @@ fn ping(host: &str) {
                     match incoming_ip.get_next_level_protocol() {
                         IpNextHeaderProtocols::Icmp => {
                             match IcmpPacket::new(incoming_ip.payload()) {
-                                Some(_) => {
-                                    // let echo_reply = EchoReply::new(); 
-                                    println!("{} bytes from {:?}", incoming_eth_pkt.packet().len(), incoming_ip.get_source());
+                                Some(p) => {
+                                    let echo = EchoReplyPacket::new(&p.packet()).unwrap();
+                                    println!(
+                                        "Successful: {} bytes from {:?}: icmp_seq={} ttl={}", 
+                                        incoming_eth_pkt.packet().len(), 
+                                        incoming_ip.get_source(), 
+                                        echo.get_sequence_number(),
+                                        incoming_ip.get_ttl() 
+                                    );
                                     break 'receiving
                                 },
                                 None => {
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,17 @@
 use std::io::{stdin, stdout, Write};
 use clap::{CommandFactory, Error, FromArgMatches, Parser, Subcommand};
+use commands::operation;
 
-#[derive(Parser)]
-struct OptCli {
-    #[command(subcommand)]
-    command: Option<OptCommand>
-}
-
-#[derive(Parser)]
-struct ConfCli {
-    #[command(subcommand)]
-    command: Option<ConfCommand>
- }
+mod base;
 
 
-#[derive(Subcommand)]
-enum OptCommand {
-    Ping {
-        host: String
-    }
-}
-
-#[derive(Subcommand)]
-enum ConfCommand { }
-
-fn netcli_parse<P>(input: &[String]) -> Result<P, Error>  where P: Parser  {
-    let mut matches = match <P as CommandFactory>::command().try_get_matches_from_mut(input.clone()){
-        Ok(m) => {
-            m
-        }
-        Err(err) => return Err(err)
-    };
-    let res = match <P as FromArgMatches>::from_arg_matches_mut(&mut matches) {
-        Ok(res) => res,
-        Err(err) => return Err(err)
-    };
-    Ok(res)
-}
-
-mod ping {
-    use pnet::{packet::{icmp::{
-        self, destination_unreachable::IcmpCodes, echo_request::MutableEchoRequestPacket, IcmpCode, IcmpType, IcmpTypes, MutableIcmpPacket
-    }, Packet}, util::checksum};
-
-
-    fn send_request() {
-        let mut buff = [0u8; 8];
-        let mut pkt = MutableEchoRequestPacket::new(&mut buff).unwrap();
-        pkt.set_icmp_type(IcmpTypes::EchoRequest);
-        // pkt.set_icmp_code(IcmpCodes::);
-        pkt.set_identifier(0);
-        pkt.set_sequence_number(0);
-        pkt.set_checksum(checksum(pkt.packet(), 2));
-    }
-
-
+#[derive(Default)]
+enum CliMode {
+    // operation mode - only allows reading and viewing of certain configs
+    #[default]
+    Opr,    
+    // configuration mode - allows editing of configs
+    Conf
 }
 
 
@@ -67,8 +24,10 @@ fn main() {
             CliMode::Opr => {
                 let input = get_input(">");
 
-                let cli = match netcli_parse::<OptCli>(&input){
-                    Ok(cli) => cli,
+                let _ = match netcli_parse::<commands::operation::OptCli>(&input){
+                    Ok(cli) => {
+                        operation::execute(cli)
+                    },
                     Err(_) => {
                         println!("invalid command: {:?}", input);
                         continue 'outer
@@ -77,9 +36,9 @@ fn main() {
 
             }
             CliMode::Conf =>  {
-                let mut input = get_input("#");
+                let input = get_input("#");
 
-                let cli = match netcli_parse::<ConfCli>(&input){
+                match netcli_parse::<commands::configuration::ConfCli>(&input){
                     Ok(cli) => cli,
                     Err(_) => {
                         
@@ -92,15 +51,81 @@ fn main() {
     }
 }
 
+mod commands {
+    pub(crate) mod operation {
+        use std::io::Result;
 
-#[derive(Default)]
-enum CliMode {
-    // operation mode - only allows reading and viewing of certain configs
-    #[default]
-    Opr,    
-    // configuration mode - allows editing of configs
-    Conf
+        // holds and executes commands when on operation mode
+        use clap::{Parser, Subcommand};
+
+        use crate::base::icmp;
+
+        #[derive(Parser)]
+        pub(crate) struct OptCli {
+            #[command(subcommand)]
+            command: Option<OptCommand>
+        }
+
+        #[derive(Subcommand)]
+        enum OptCommand {
+            Ping {
+                host: String
+            }
+        }
+
+        pub(crate) fn execute(cli: OptCli) -> Result<()> {
+
+            match cli.command {
+                Some(command) => {
+                    match command {
+                        OptCommand::Ping { host } => {
+                            icmp::ping(&host);
+                            Ok(())
+                        }
+                    }
+                }
+                None => Ok(())
+            }
+        }
+
+
+    }
+
+    // todo
+    pub(crate) mod configuration {  
+        use clap::{Parser, Subcommand};
+
+        #[derive(Parser)]
+        pub(crate) struct ConfCli {
+            #[command(subcommand)]
+            command: Option<CliCommand>
+        }
+
+        #[derive(Subcommand)]
+        enum CliCommand {
+
+        }
+
+        
+    }
 }
+
+fn netcli_parse<P>(input: &[String]) -> Result<P, Error>  where P: Parser  {
+    let mut matches = match <P as CommandFactory>::command().try_get_matches_from_mut(input){
+        Ok(m) => {
+            m
+        }
+        Err(err) => return Err(err)
+    };
+    let res = match <P as FromArgMatches>::from_arg_matches_mut(&mut matches) {
+        Ok(res) => res,
+        Err(err) => return Err(err)
+    };
+    Ok(res)
+}
+
+
+
 
 
 
@@ -115,3 +140,4 @@ fn get_input(bash: &str) -> Vec<String> {
     input.split(" ").into_iter().for_each(|a| args.push(a.trim().to_string()));
     args
 }
+
